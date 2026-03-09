bef029cc2c014208c586d8dc0f812b0baf2d4451
Benedictkay <kalelib88@gmail.com>
2026-03-09 12:36:44 +0300

    code reformat

--- PMC-patient-footfall-forecast-lr/app.py
+++ PMC-patient-footfall-forecast-lr/app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import joblib
+from pathlib import Path
 
 # Get the directory where app.py is located
 curr_dir = Path(__file__).parent
